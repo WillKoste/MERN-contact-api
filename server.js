6e5897623e83bc60aff389cb12fa45fe39dba570
Will Koste <willkoste@gmail.com>
2020-03-31 10:07:12 -0400

    User Model & Register A User Route

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 
 connectDB();
 
+// Body Parser Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => {
   res.json({msg: 'Welcome to your brand new Contact API'});
 });
