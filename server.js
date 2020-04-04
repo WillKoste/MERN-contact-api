ed32112f9400db761b56617b11247b0e1e3fd48a
Will Koste <willkoste@gmail.com>
2020-04-04 16:22:49 -0400

    Ready For Deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const contacts = require('./routes/contacts');
+const path = require('path');
 
 const app = express();
 
@@ -11,13 +12,17 @@ connectDB();
 // Body Parser Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => {
-  res.json({msg: 'Welcome to your brand new Contact API'});
-});
-
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/contacts', contacts);
+
+// Serve Static Assets In Production
+if(process.env.NODE_ENV === 'production'){
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
